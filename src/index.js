92bc63ca61543530946d79ec57f0c8db4600a9ee
Julian Baldwin <julianbaldwin97@gmail.com>
2022-10-14 12:03:31 -0500

    trying without app.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import 'popper.js';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+// import App from './App';
+import {Page} from "./gui/pages/page";
 
-const root = ReactDOM.unstable_createRoot(document.getElementById('root'));
-root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
-);
+
+// const root = ReactDOM.unstable_createRoot(document.getElementById('root'));
+// root.render(
+// 	<BrowserRouter>
+// 		<App />
+// 	</BrowserRouter>
+// );
 
 // ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
+
+ReactDOM.render(<Page />, document.getElementById('root'));
