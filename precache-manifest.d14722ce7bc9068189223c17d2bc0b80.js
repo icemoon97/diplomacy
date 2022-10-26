0ce64a2aadc56348e817865cac5befcb06d658cc
Julian Baldwin <julianbaldwin97@gmail.com>
2022-10-26 00:30:59 -0500

    Updates

---
+++ precache-manifest.d14722ce7bc9068189223c17d2bc0b80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764c75a8ebd00ef88e59fbc0aed2ce5a",
+    "url": "/diplomacy-defcon-tool/index.html"
+  },
+  {
+    "revision": "f6edf709ca7a10a7783a",
+    "url": "/diplomacy-defcon-tool/static/css/2.d64b8b57.chunk.css"
+  },
+  {
+    "revision": "3abb042e276df6e07f55",
+    "url": "/diplomacy-defcon-tool/static/css/main.19c040f4.chunk.css"
+  },
+  {
+    "revision": "f6edf709ca7a10a7783a",
+    "url": "/diplomacy-defcon-tool/static/js/2.40df2dad.chunk.js"
+  },
+  {
+    "revision": "3abb042e276df6e07f55",
+    "url": "/diplomacy-defcon-tool/static/js/main.0787f2b5.chunk.js"
+  },
+  {
+    "revision": "2739591d5dfed8f4dba1",
+    "url": "/diplomacy-defcon-tool/static/js/runtime~main.cea27317.js"
+  },
+  {
+    "revision": "34cd4139a6a9809e23eedf4bc93ff8b6",
+    "url": "/diplomacy-defcon-tool/static/media/ancmed.34cd4139.svg"
+  },
+  {
+    "revision": "fbebe33caf7bd7127a2f26742a4f2353",
+    "url": "/diplomacy-defcon-tool/static/media/modern.fbebe33c.svg"
+  },
+  {
+    "revision": "2b44afcd3fdb108d5132905516f29260",
+    "url": "/diplomacy-defcon-tool/static/media/pure.2b44afcd.svg"
+  },
+  {
+    "revision": "79c1ccc8d1c389be30f0a61541af5e1e",
+    "url": "/diplomacy-defcon-tool/static/media/standard.79c1ccc8.svg"
+  }
+]);
