9d3feee6f8c4f88372e9ecd2578241020b133ea0
Julian Baldwin <julianbaldwin97@gmail.com>
2022-11-15 16:27:16 -0600

    more descriptive file name

--- diplomacy/web/src/gui/utils/defconSaveGame.js
+++ diplomacy/web/src/gui/utils/defconSaveGame.js
@@ -8,7 +8,7 @@ export function saveDefconLabels(game, labels) {
     const domLink = document.createElement('a');
     domLink.setAttribute(
         'href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(labels)));
-    domLink.setAttribute('download', `${labels.game_id}.json`);
+    domLink.setAttribute('download', `DEFCON labels - id:${labels.game_id}.json`);
     domLink.style.display = 'none';
     document.body.appendChild(domLink);
     domLink.click();
